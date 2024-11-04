0dc8d95c7face86f3d321152f65baecfaf1dab92
Abril Jordan Casinillo <abriljordancasinillo@gmail.com>
2024-11-04 15:16:25 +0800

    updated the dashboard

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+  //= link admin/dashboard.css
+  //= link admin/movies.css
+  //= link admin/showtimes.css
